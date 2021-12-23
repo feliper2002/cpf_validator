018bd909af605a3d4b9295cd69501f3a51e74c46
Felipe Ribeiro <feliper.dev@gmail.com>
2021-12-23 14:20:27 -0300

    Refactoring code

--- lib/home/model/validate_cpf.dart
+++ lib/home/model/validate_cpf.dart
@@ -3,7 +3,7 @@ abstract class ValidateCPF {
 }
 
 class ValidateCPFImpl implements ValidateCPF {
-  bool _resto(int digito, List<int> cpf) {
+  bool _resto(int digito, String cpf) {
     /// Aplies formula that [validate] parsed cpf
     ///
     /// - [digito]: numeric [index] at CPF string
@@ -12,7 +12,7 @@ class ValidateCPFImpl implements ValidateCPF {
     int soma = 0;
     for (var k = 0; k <= (digito - 2);) {
       for (var i = digito; i >= 2; i--) {
-        soma += (cpf[k] * i);
+        soma += (int.parse(cpf[k]) * i);
         k++;
       }
     }
@@ -21,7 +21,7 @@ class ValidateCPFImpl implements ValidateCPF {
 
     if (resto == 10) resto = 0;
 
-    return (resto == cpf[digito - 1]);
+    return (resto == int.parse(cpf[digito - 1]));
   }
 
   @override
@@ -38,17 +38,8 @@ class ValidateCPFImpl implements ValidateCPF {
 
     if (errors.isNotEmpty) return false;
 
-    final cpfList = <int>[];
-
-    for (var i = 0; i < cpf.length; i++) {
-      /// Parse all CPF string unique char at an [int] by [String extension]
-      ///
-      /// Add respective CPF char as int at [cpfList]
-      cpfList.add(int.parse(cpf[i]));
-    }
-
-    bool decimo = _resto(10, cpfList);
-    bool decimoPrimeiro = _resto(11, cpfList);
+    bool decimo = _resto(10, cpf);
+    bool decimoPrimeiro = _resto(11, cpf);
 
     return (decimo && decimoPrimeiro);
   }
